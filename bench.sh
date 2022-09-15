b6b09ab84e14aa78d7ffa5d805f833342d7788a8
Tavian Barnes <tavianator@tavianator.com>
2022-09-15 16:50:52 -0400

    Make it work on Intel too

--- bench.sh
+++ bench.sh
@@ -7,8 +7,17 @@ IMPLS=(baseline {in,ex}clusive)
 THREADS=(1 2 4 6 8 12 16 24 32 48)
 LEVELS=({4..10})
 
+GOVERNOR=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 sudo cpupower frequency-set -g performance >/dev/null
-echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost >/dev/null
+
+INTEL_BOOST=/sys/devices/system/cpu/intel_pstate/no_turbo
+OTHER_BOOST=/sys/devices/system/cpu/cpufreq/boost
+
+if [ -e "$INTEL_BOOST" ]; then
+    echo 1 | sudo tee "$INTEL_BOOST" >/dev/null
+else
+    echo 0 | sudo tee "$OTHER_BOOST" >/dev/null
+fi
 
 printf '{\n'
 
@@ -39,5 +48,10 @@ done
 
 printf '\n}\n'
 
-sudo cpupower frequency-set -g schedutil >/dev/null
-echo 1 | sudo tee /sys/devices/system/cpu/cpufreq/boost >/dev/null
+sudo cpupower frequency-set -g "$GOVERNOR" >/dev/null
+
+if [ -e "$INTEL_BOOST" ]; then
+    echo 0 | sudo tee "$INTEL_BOOST" >/dev/null
+else
+    echo 1 | sudo tee "$OTHER_BOOST" >/dev/null
+fi
